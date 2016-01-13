aea9bf9184c6af85ff15ca2edb4db04bb21e3586
Mike <mfo0815@gmail.com>
2016-01-12 19:52:35 -0500

    changes made to add email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.sendgrid.net',
+    address:        'smtp.gmail.com',
     port:           '587',
     authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'heroku.com',
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    domain:         'gmail.com',
     enable_starttls_auto: true
   }
 end
